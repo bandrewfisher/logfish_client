92e717bf92ff0d991cb9879360fd2555c2443ab8
Brett Fisher <brett.fisher@endurance.com>
2020-04-26 11:08:13 -0600

    No more hardcoded server endpoint

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
 
 // const socket = io.connect('http://localhost:8081');
-const socket = io.connect(process.env.LOGFISH_SERVER_URL || 'http://logfish.us-west-2.elasticbeanstalk.com/');
+const socket = io.connect(process.env.LOGFISH_SERVER_URL || 'http://localhost:8081');
 export default socket;
